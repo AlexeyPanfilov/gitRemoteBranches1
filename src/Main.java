d1779535ffec67bba045c90f27562232385993e0
Alexey Panfilov <ap83@list.ru>
2022-07-25 10:59:30 +0300

    Metrika added

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] sales = {5, 13, 6, 8, 9};
-        SalesManager manager = new SalesManager(sales);
-
-        System.out.println(manager.max());
+        System.out.println("Metrika");
     }
 }
